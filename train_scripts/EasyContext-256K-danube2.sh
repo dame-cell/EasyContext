d078446a7f8fb4e84bc6d1bfb8f8dcafcf6910f4
DAME RAJEE <122996026+dame-cell@users.noreply.github.com>
2024-05-09 21:20:39 +0530

    Update EasyContext-256K-danube2.sh

--- train_scripts/EasyContext-256K-danube2.sh
+++ train_scripts/EasyContext-256K-danube2.sh
@@ -2,21 +2,20 @@
 # HF's mistral architecture is off when used together with ring attention due to the way it implements rotary embedding. Llama has no such issue.
 
 
-accelerate launch train.py \
---config_file  accelerate_configs/single_node.yaml \
---main_process_port 12345 \
+accelerate launch --config_file accelerate_configs/single_node.yaml --main_process_port 12345 train.py \
 --batch-size 1 \
 --gradient-accumulate-every 4 \
---output-dir  output \
+--output-dir ./output/h2o \
 --wandb EasyContext \
 --seed 2026 \
---max-train-steps 400  \
---learning-rate 2e-5  \
+--max-train-steps 400 \
+--learning-rate 2e-5 \
 --dataset PY007/slimpajama_mistral_tokenized_upsample_4096_chunk_128K \
---model output/h2o \
+--model output \
 --seq-length 32000 \
 --rope-theta 300000 \
 --parallel_mode data_parallel
+
 
 
 
